57bdf62864fa22378724c751b93e014157d7075c
anddhong <andrewh1@andrew.cmu.edu>
2020-03-25 22:32:24 -0400

    added the rest of the javascript

--- HW6/main.js
+++ HW6/main.js
@@ -1,8 +1,9 @@
 
 // Opens popup box
-function openModal() {
+function openModal(flavor) {
 	var modal = document.getElementById("modal")
 	modal.style.display = "block";
+	sessionStorage.setItem("flavor", flavor)
 }
 
 // Closes popup box
@@ -13,10 +14,42 @@ function closeModal() {
 
 // Updates badge and exits modal app
 function addToCart() {
-	var modal = document.getElementById("modal")
-	modal.style.display = "none"
-	var badge = document.getElementById("badge")
-	badge.innerHTML = parseInt(badge.innerHTML) + 1
+	document.getElementById("modal").style.display = "none"
+	var items = sessionStorage.getItem('items')
+	if (items===null) {
+		items = {}
+	} else {
+		items = JSON.parse(items)
+	}
+	var name = sessionStorage.getItem('flavor')
+	var quantity = document.getElementById("quantity")
+	quantity = quantity.options[quantity.selectedIndex].value
+	var icing = document.getElementById("icing")
+
+	var bun = name + ' ' + icing.options[icing.selectedIndex].text
+
+	var currentNum = 0
+	if (items[bun]!=null) {
+		currentNum = items[bun]
+	}
+	items[bun] = parseInt(quantity) + parseInt(currentNum)
+	sessionStorage.setItem('items',JSON.stringify(items))
+
+	sessionStorage.setItem('cartSize', parseInt(sessionStorage.getItem('cartSize')) + parseInt(quantity))
+	updateCartSize()
+	console.log(sessionStorage)
+}
+
+window.onload = function() {
+	updateCartSize()
+}
+
+function updateCartSize() {
+	var currentSize = sessionStorage.getItem('cartSize')
+	if (currentSize===null) {
+		sessionStorage.setItem('cartSize',0)
+	}
+	document.getElementById("badge").innerHTML = sessionStorage.getItem('cartSize')
 }
 
 // Clicking outside also closes mode
