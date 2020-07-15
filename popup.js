8b1f94faada5460aa9acbb72786acc3fe81a7312
anddhong <andrewh1@andrew.cmu.edu>
2020-07-15 00:10:12 -0400

    expose

---
+++ popup.js
@@ -0,0 +1,22 @@
+console.log('hello')
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
+
+//       var f = d.createElement('form');
+//       f.action = 'file:///C:/Users/Andrew/Documents/chromeExt/article.html';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
+
