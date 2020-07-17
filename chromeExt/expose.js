7dd98ea0d5ad80b9817bd53a04bff59009c14afe
anddhong <andrewh1@andrew.cmu.edu>
2020-07-17 02:40:46 -0400

    attempt 3 to transfer URL

--- chromeExt/expose.js
+++ chromeExt/expose.js
@@ -2,7 +2,7 @@
 $(window).on('load', function() {
 	console.log('hellosa')
 	// var url = window.opener.url;
-	console.log(url)
+	console.log()
 	console.log(this.url)
 	console.log(window.url)
  });
